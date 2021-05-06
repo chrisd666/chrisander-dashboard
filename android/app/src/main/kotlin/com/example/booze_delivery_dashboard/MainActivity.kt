833214b75fd9ff45ec355351185d7ee2e9b75bbd
chrisD666 <chris.dias@lotusis.com>
2021-05-06 22:55:58 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/booze_delivery_dashboard/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.booze_delivery_dashboard
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/main/main_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Row(
+          children: [
+            Expanded(
+                child: Container(
+              color: Colors.white,
+            )),
+            Expanded(
+                flex: 5,
+                child: Container(
+                  color: Colors.blue,
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
