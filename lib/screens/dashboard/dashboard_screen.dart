98724a8c7a9d6b6c0a6214e005484da99f570a25
chrisD666 <chris.dias@lotusis.com>
2021-05-08 17:31:48 +0530

    chart

--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../../constants.dart';
 import './components/header.dart';
+import './components/chart.dart';
 
 class DashboardScreen extends StatelessWidget {
   @override
@@ -11,6 +12,45 @@ class DashboardScreen extends StatelessWidget {
         child: Column(
           children: [
             Header(),
+            SizedBox(
+              width: defaultPadding,
+            ),
+            Row(
+              children: [
+                Expanded(
+                    flex: 5,
+                    child: Container(
+                      height: 500,
+                      color: Colors.white,
+                    )),
+                SizedBox(
+                  width: defaultPadding,
+                ),
+                Expanded(
+                    flex: 2,
+                    child: Container(
+                      padding: EdgeInsets.all(defaultPadding),
+                      height: 500,
+                      decoration: BoxDecoration(
+                          color: secondaryColor,
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(10))),
+                      child: Column(
+                        children: [
+                          Text(
+                            "Storage Details",
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.w500),
+                          ),
+                          SizedBox(
+                            height: defaultPadding,
+                          ),
+                          Chart()
+                        ],
+                      ),
+                    ))
+              ],
+            )
           ],
         ),
       ),
